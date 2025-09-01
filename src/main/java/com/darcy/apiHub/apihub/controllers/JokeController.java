5541f2ece8a3dbaac5fc96a7130530b863d25c0c
DarcyGeyer <95901667+darcyg32@users.noreply.github.com>
2025-09-01 14:46:00 +0930

    Implemented JokeController

--- src/main/java/com/darcy/apiHub/apihub/controllers/JokeController.java
+++ src/main/java/com/darcy/apiHub/apihub/controllers/JokeController.java
@@ -1,4 +1,22 @@
 package com.darcy.apiHub.apihub.controllers;
 
+import com.darcy.apiHub.apihub.dtos.JokeDTO;
+import com.darcy.apiHub.apihub.services.JokeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/joke")
 public class JokeController {
+
+    private final JokeService service;
+
+    public JokeController(JokeService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<JokeDTO> random() {
+        return ResponseEntity.ok(service.randomJoke());
+    }
 }
