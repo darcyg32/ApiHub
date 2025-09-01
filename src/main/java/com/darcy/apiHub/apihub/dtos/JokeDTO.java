f35900e605f312c619193bfa87120c9f4691a3f8
DarcyGeyer <95901667+darcyg32@users.noreply.github.com>
2025-09-01 13:46:00 +0930

    Implemented JokeDTO

--- src/main/java/com/darcy/apiHub/apihub/dtos/JokeDTO.java
+++ src/main/java/com/darcy/apiHub/apihub/dtos/JokeDTO.java
@@ -1,4 +1,19 @@
 package com.darcy.apiHub.apihub.dtos;
 
 public class JokeDTO {
+    private String setup;
+    private String punchline;
+
+    public JokeDTO() {}
+
+    public JokeDTO(String setup, String punchline) {
+        this.setup = setup;
+        this.punchline = punchline;
+    }
+
+    public String getSetup() { return this.setup; }
+    public void setSetup(String setup) { this.setup = setup; }
+
+    public String getPunchline() { return this.punchline; }
+    public String setPunchline(String punchline) { this.punchline = punchline; }
 }
