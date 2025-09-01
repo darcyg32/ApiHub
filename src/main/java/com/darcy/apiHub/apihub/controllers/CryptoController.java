132f531bf4a47e7bf4b3f763642cac04613b0d00
DarcyGeyer <95901667+darcyg32@users.noreply.github.com>
2025-09-01 14:49:51 +0930

    Implemented CryptoController

--- src/main/java/com/darcy/apiHub/apihub/controllers/CryptoController.java
+++ src/main/java/com/darcy/apiHub/apihub/controllers/CryptoController.java
@@ -1,4 +1,22 @@
 package com.darcy.apiHub.apihub.controllers;
 
+import com.darcy.apiHub.apihub.dtos.CryptoPriceDTO;
+import com.darcy.apiHub.apihub.services.CryptoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/crypto")
 public class CryptoController {
+
+    private final CryptoService service;
+
+    public CryptoController(CryptoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<CryptoPriceDTO> current() {
+        return ResponseEntity.ok(service.getBTCandETHinAUD());
+    }
 }
