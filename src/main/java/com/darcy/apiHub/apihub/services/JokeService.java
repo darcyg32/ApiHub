0f0ffb929d2fa426ede3f4cf6673f9eef42207d4
DarcyGeyer <95901667+darcyg32@users.noreply.github.com>
2025-09-01 14:26:17 +0930

    Implemented JokeService

--- src/main/java/com/darcy/apiHub/apihub/services/JokeService.java
+++ src/main/java/com/darcy/apiHub/apihub/services/JokeService.java
@@ -1,4 +1,23 @@
 package com.darcy.apiHub.apihub.services;
 
+import com.darcy.apiHub.apihub.clients.OfficialJokeClient;
+import com.darcy.apiHub.apihub.dtos.JokeDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JokeService {
+
+    private final OfficialJokeClient jokeClient;
+
+    public JokeService(OfficialJokeClient jokeClient) {
+        this.jokeClient = jokeClient;
+    }
+
+    public JokeDTO randomJoke() {
+        var resp = jokeClient.getRandomJoke();
+        return new JokeDTO(
+                resp != null ? resp.setup : null,
+                resp != null ? resp.punchline : null
+        );
+    }
 }
